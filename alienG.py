4513aedb0bd0dba02125cb17b669481f664b8fd4
Bruno Ortolan <bru.ortolan@hotmail.com>
2022-06-19 17:26:45 -0300

    Adicionado missil e o seu respawn.

--- alienG.py
+++ alienG.py
@@ -21,12 +21,22 @@ playerImg = pygame.image.load('assets/space.png').convert_alpha()
 playerImg = pygame.transform.scale(playerImg, (50,50))
 playerImg = pygame.transform.rotate(playerImg, -90)
 
+missil = pygame.image.load('assets/missile.png').convert_alpha()
+missil = pygame.transform.scale(missil, (25,25))
+missil = pygame.transform.rotate(missil, -45)
+
 pos_alien_x = 500
 pos_alien_y = 360
 
 pos_player_x = 200
 pos_player_y = 300
 
+vel_missil_x = 0
+pos_missil_x = 200
+pos_missil_y = 300
+
+
+triggered = False
 rodando = True
 
 #FUNÇÕES
@@ -34,6 +44,12 @@ def respawn():
     x = 1350
     y = random.randint(1,640)
     return[x,y]
+def respawn_missil():
+    triggered = False
+    respawn_missil_x = pos_player_x
+    respawn_missil_y = pos_player_y
+    vel_x_missil = 0
+    return [respawn_missil_x, respawn_missil_y, triggered, vel_x_missil]
 
 
 while rodando:
@@ -52,21 +68,35 @@ while rodando:
     tecla = pygame.key.get_pressed()
     if tecla[pygame.K_UP] and pos_player_y > 1:
         pos_player_y -= 8
+        if not triggered:
+            pos_missil_y -= 8
     
     if tecla[pygame.K_DOWN] and pos_player_y < 665:
         pos_player_y += 8
+        if not triggered:
+            pos_missil_y += 8
+
+    if tecla[pygame.K_SPACE]:
+        triggered = True
+        vel_missil_x = 20
 
     #RESPAWN
     if pos_alien_x == 50:
         pos_alien_x = respawn()[0]
         pos_alien_y = respawn()[1]
+    
+    if pos_missil_x == 1300:
+        pos_missil_x, pos_missil_y, triggered, vel_missil_x = respawn_missil()
 
     #MOVIMENTO
     x -= 5
     pos_alien_x -= 10
 
+    pos_missil_x += vel_missil_x
+
     #CRIAR IMAGENS
     screen.blit(alien, (pos_alien_x, pos_alien_y))
+    screen.blit(missil, (pos_missil_x, pos_missil_y))
     screen.blit(playerImg, (pos_player_x, pos_player_y))
 
     pygame.display.update()
